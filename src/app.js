65984643a57f5bbea82c8430c3c39d3769f8a86b
moises2872024 <moises.montt287@gmail.com>
2024-11-04 15:15:04 +0000

    generador finalizado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let first = "A ";
+  let caract = ["angry ", "friendly ", "crazy ", "slow "];
+  let who = ["granny ", "dog ", "woman ", "turtle "];
+  let action = ["say ", "ran ", "stole ", "almost "];
+  let what = [
+    "that we can't play here",
+    "towards me",
+    "my shopping cart",
+    "got hit by a car"
+  ];
+
+  let var1 = Math.floor(Math.random() * caract.length);
+  let var2 = Math.floor(Math.random() * who.length);
+  let var3 = Math.floor(Math.random() * action.length);
+  let var4 = Math.floor(Math.random() * what.length);
+
+  document.querySelector("#generator").innerHTML =
+    first + caract[var1] + who[var2] + action[var3] + what[var4];
 };
